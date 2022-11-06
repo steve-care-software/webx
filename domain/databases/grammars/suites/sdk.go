c8f98b156d026d665e1952f4e61760351524e798
Steve Rodrigue <stev.rodr@gmail.com>
2022-11-06 12:47:27 -0500

    Added the database suite structs

--- domain/databases/grammars/suites/sdk.go
+++ domain/databases/grammars/suites/sdk.go
@@ -2,6 +2,11 @@ package suites
 
 import "github.com/steve-care-software/webx/domain/databases/entities"
 
+// NewBuilder creates a new builder instance
+func NewBuilder() Builder {
+	return createBuilder()
+}
+
 // Builder represents a suite builder
 type Builder interface {
 	Create() Builder
