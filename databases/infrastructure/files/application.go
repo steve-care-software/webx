a3a0ff7486e0209436141a306ddd28c84d4a4f81
Steve Rodrigue <stev.rodr@gmail.com>
2022-12-20 04:09:36 -0500

    Added the List method on the database application

--- databases/infrastructure/files/application.go
+++ databases/infrastructure/files/application.go
@@ -93,6 +93,25 @@ func createApplication(
 	return &out
 }
 
+// List returns the current database names
+func (app *application) List() ([]string, error) {
+	files, err := ioutil.ReadDir(app.dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	list := []string{}
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		list = append(list, file.Name())
+	}
+
+	return list, nil
+}
+
 // Exists returns true if the database exists, false otherwise
 func (app *application) Exists(name string) bool {
 	path := filepath.Join(app.dirPath, name)
