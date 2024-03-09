cfc5c8e1593c77487d8ba782d7fc181cc2a87198
Steve Rodrigue <stev.rodr@gmail.com>
2024-03-08 21:44:44 -0500

    Added the compiler application

--- applications/libraries/compilers/application.go
+++ applications/libraries/compilers/application.go
@@ -3,19 +3,25 @@ package compilers
 import "github.com/steve-care-software/datastencil/domain/libraries"
 
 type application struct {
+	adapter libraries.Adapter
 }
 
-func createApplication() Application {
-	out := application{}
+func createApplication(
+	adapter libraries.Adapter,
+) Application {
+	out := application{
+		adapter: adapter,
+	}
+
 	return &out
 }
 
 // Compile compiles the application
 func (app *application) Compile(input []byte) (libraries.Library, error) {
-	return nil, nil
+	return app.adapter.ToInstance(input)
 }
 
 // Decompile decompiles the application
 func (app *application) Decompile(library libraries.Library) ([]byte, error) {
-	return nil, nil
+	return app.adapter.ToData(library)
 }
