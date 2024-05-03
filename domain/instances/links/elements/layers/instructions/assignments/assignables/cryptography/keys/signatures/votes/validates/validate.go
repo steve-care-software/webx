74c870251cec741ab4fbfd1af6920b15b5867552
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-03 08:07:38 -0400

    Added the vote validate struct

---
+++ domain/instances/links/elements/layers/instructions/assignments/assignables/cryptography/keys/signatures/votes/validates/validate.go
@@ -0,0 +1,46 @@
+package validates
+
+import "github.com/steve-care-software/datastencil/domain/hash"
+
+type validate struct {
+	hash       hash.Hash
+	vote       string
+	message    string
+	hashedRing string
+}
+
+func createValidate(
+	hash hash.Hash,
+	vote string,
+	message string,
+	hashedRing string,
+) Validate {
+	out := validate{
+		hash:       hash,
+		vote:       vote,
+		message:    message,
+		hashedRing: hashedRing,
+	}
+
+	return &out
+}
+
+// Hash returns the hash
+func (obj *validate) Hash() hash.Hash {
+	return obj.hash
+}
+
+// Vote returns the vote
+func (obj *validate) Vote() string {
+	return obj.vote
+}
+
+// Message returns the message
+func (obj *validate) Message() string {
+	return obj.message
+}
+
+// HashedRing returns the hashed ring
+func (obj *validate) HashedRing() string {
+	return obj.hashedRing
+}
