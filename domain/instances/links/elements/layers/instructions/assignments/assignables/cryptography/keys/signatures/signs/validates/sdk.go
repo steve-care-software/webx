40d5b9ac943644c79f143e4c5b8b1b9eca1dba13
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-03 05:50:04 -0400

    Added the signature validation structs

--- domain/instances/links/elements/layers/instructions/assignments/assignables/cryptography/keys/signatures/signs/validates/sdk.go
+++ domain/instances/links/elements/layers/instructions/assignments/assignables/cryptography/keys/signatures/signs/validates/sdk.go
@@ -4,11 +4,19 @@ import (
 	"github.com/steve-care-software/datastencil/domain/hash"
 )
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
 // Builder represents a validate builder
 type Builder interface {
 	Create() Builder
 	WithSignature(sig string) Builder
-	WithMessage(msg Builder) Builder
+	WithMessage(msg string) Builder
 	WithPublicKey(pubKey string) Builder
 	Now() (Validate, error)
 }
