d2c9416759fa82950ebfc719ca025461a91148d0
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-07 08:53:17 -0400

    Added the database structs

---
+++ domain/instances/links/elements/layers/instructions/assignments/assignables/databases/databases/builder.go
@@ -0,0 +1,93 @@
+package databases
+
+import (
+	"errors"
+
+	"github.com/steve-care-software/datastencil/domain/hash"
+)
+
+type builder struct {
+	hashAdapter hash.Adapter
+	path        string
+	description string
+	head        string
+	isActive    bool
+}
+
+func createBuilder(
+	hashAdapter hash.Adapter,
+) Builder {
+	out := builder{
+		hashAdapter: hashAdapter,
+		path:        "",
+		description: "",
+		head:        "",
+		isActive:    false,
+	}
+
+	return &out
+}
+
+// Create initializes the builder
+func (app *builder) Create() Builder {
+	return createBuilder(
+		app.hashAdapter,
+	)
+}
+
+// WithPath adds a path to the builder
+func (app *builder) WithPath(path string) Builder {
+	app.path = path
+	return app
+}
+
+// WithDescription adds a description to the builder
+func (app *builder) WithDescription(description string) Builder {
+	app.description = description
+	return app
+}
+
+// WithHead adds a head to the builder
+func (app *builder) WithHead(head string) Builder {
+	app.head = head
+	return app
+}
+
+// IsActive flags the builder as active
+func (app *builder) IsActive() Builder {
+	app.isActive = true
+	return app
+}
+
+// Now builds a new Database
+func (app *builder) Now() (Database, error) {
+	if app.path == "" {
+		return nil, errors.New("the path is mandatory in order to build a Database instance")
+	}
+
+	if app.description == "" {
+		return nil, errors.New("the description is mandatory in order to build a Database instance")
+	}
+
+	if app.head == "" {
+		return nil, errors.New("the head is mandatory in order to build a Database instance")
+	}
+
+	isActive := "false"
+	if app.isActive {
+		isActive = "true"
+	}
+
+	pHash, err := app.hashAdapter.FromMultiBytes([][]byte{
+		[]byte(app.path),
+		[]byte(app.description),
+		[]byte(app.head),
+		[]byte(isActive),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return createDatabase(*pHash, app.path, app.description, app.head, app.isActive), nil
+}
--- domain/instances/links/elements/layers/instructions/assignments/assignables/databases/databases/sdk.go
+++ domain/instances/links/elements/layers/instructions/assignments/assignables/databases/databases/sdk.go
@@ -2,10 +2,28 @@ package databases
 
 import "github.com/steve-care-software/datastencil/domain/hash"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the database builder
+type Builder interface {
+	Create() Builder
+	WithPath(path string) Builder
+	WithDescription(description string) Builder
+	WithHead(head string) Builder
+	IsActive() Builder
+	Now() (Database, error)
+}
+
 // Database represents a database
 type Database interface {
 	Hash() hash.Hash
-	Path() []string
+	Path() string
 	Description() string
 	Head() string
 	IsActive() bool
