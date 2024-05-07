17aa4231249d615810bcc7429d0566b295e44724
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-07 09:08:34 -0400

    Added the retrieve structs

--- domain/instances/links/elements/layers/instructions/assignments/assignables/databases/retrieves/sdk.go
+++ domain/instances/links/elements/layers/instructions/assignments/assignables/databases/retrieves/sdk.go
@@ -2,6 +2,23 @@ package retrieves
 
 import "github.com/steve-care-software/datastencil/domain/hash"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents a retrieve builder
+type Builder interface {
+	Create() Builder
+	WithExists(exists string) Builder
+	WithRetrieve(retrieve string) Builder
+	IsList() Builder
+	Now() (Retrieve, error)
+}
+
 // Retrieve represents a retrieve
 type Retrieve interface {
 	Hash() hash.Hash
