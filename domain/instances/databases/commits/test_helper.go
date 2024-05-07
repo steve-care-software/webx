720098d59582637f58ce78d64f87c49f1a9e788e
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-07 04:13:45 -0400

    Added the commit test helper

---
+++ domain/instances/databases/commits/test_helper.go
@@ -0,0 +1,38 @@
+package commits
+
+import (
+	"time"
+
+	"github.com/steve-care-software/datastencil/domain/instances/databases/commits/actions"
+)
+
+// NewCommitForTests creates a new commit for tests
+func NewCommitForTests(description string, actions actions.Actions, createdOn time.Time) Commit {
+	ins, err := NewBuilder().Create().
+		WithDescription(description).
+		WithActions(actions).
+		CreatedOn(createdOn).
+		Now()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return ins
+}
+
+// NewCommitWithParentForTests creates a new commit with parent for tests
+func NewCommitWithParentForTests(description string, actions actions.Actions, createdOn time.Time, parent Commit) Commit {
+	ins, err := NewBuilder().Create().
+		WithDescription(description).
+		WithActions(actions).
+		CreatedOn(createdOn).
+		WithParent(parent).
+		Now()
+
+	if err != nil {
+		panic(err)
+	}
+
+	return ins
+}
