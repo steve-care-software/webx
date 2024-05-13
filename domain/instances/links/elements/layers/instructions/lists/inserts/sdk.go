451a25f1b4c67de788a28f613cdb40a920a679f3
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-13 16:56:49 -0400

    Added the insert struct

--- domain/instances/links/elements/layers/instructions/lists/inserts/sdk.go
+++ domain/instances/links/elements/layers/instructions/lists/inserts/sdk.go
@@ -2,6 +2,14 @@ package inserts
 
 import "github.com/steve-care-software/datastencil/domain/hash"
 
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
 // Builder represents an insert builder
 type Builder interface {
 	Create() Builder
