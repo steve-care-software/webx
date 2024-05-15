2b93b4b8454a9eb3a9183876ae02707117b87b19
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-15 07:27:07 -0400

    Added the list json adapter

---
+++ infrastructure/jsons/instances/links/elements/layers/instructions/lists/sdk.go
@@ -0,0 +1,19 @@
+package lists
+
+import (
+	"github.com/steve-care-software/datastencil/domain/instances/links/elements/layers/instructions/lists"
+	json_deletes "github.com/steve-care-software/datastencil/infrastructure/jsons/instances/links/elements/layers/instructions/lists/deletes"
+	json_inserts "github.com/steve-care-software/datastencil/infrastructure/jsons/instances/links/elements/layers/instructions/lists/inserts"
+)
+
+// NewAdapter creates a new adapter
+func NewAdapter() lists.Adapter {
+	deleteAdapter := json_deletes.NewAdapter()
+	insertAdapter := json_inserts.NewAdapter()
+	builder := lists.NewBuilder()
+	return createAdapter(
+		deleteAdapter.(*json_deletes.Adapter),
+		insertAdapter.(*json_inserts.Adapter),
+		builder,
+	)
+}
