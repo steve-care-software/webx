4111787d28d2faece1bf3aaeca30fb4a990ed7b1
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-18 15:36:52 -0400

    Removed unnecessary failure codes

--- domain/stacks/failures/sdk.go
+++ domain/stacks/failures/sdk.go
@@ -1,41 +1,8 @@
 package failures
 
 const (
-	// CouldNotInsertAccountInDatabase represents a could not insert account in database
-	CouldNotInsertAccountInDatabase (uint) = iota
-
-	// CouldNotUpdateAccountInDatabase represents a could not update account in database
-	CouldNotUpdateAccountInDatabase
-
-	// CouldNotDeleteAccountFromDatabase represents a could not delete account from database
-	CouldNotDeleteAccountFromDatabase
-
-	// CouldNotFetchCredentialsFromFrame represents a could not fetch credentials from frame
-	CouldNotFetchCredentialsFromFrame
-
-	// CouldNotFetchUsernameFromFrame represents a could not fetch username from frame
-	CouldNotFetchUsernameFromFrame
-
 	// CouldNotFetchPasswordFromFrame represents a could not fetch password from frame
-	CouldNotFetchPasswordFromFrame
-
-	// AccountWithSameUsernameAlreadyExists represents an account with same username already exists
-	AccountWithSameUsernameAlreadyExists
-
-	// CouldNotFetchContextFromFrame represents a could not fetch context from frame
-	CouldNotFetchContextFromFrame
-
-	// CouldNotFetchPathFromFrame represents a could not fetch path from frame
-	CouldNotFetchPathFromFrame
-
-	// CouldNotFetchIdentifierFromFrame represents a could not fetch identifier from frame
-	CouldNotFetchIdentifierFromFrame
-
-	// InstanceDoesNotExistsInDatabase represents an instance does not exists in database
-	InstanceDoesNotExistsInDatabase
-
-	// InstanceAlreadyExistsInDatabase represents an instance already exists in database
-	InstanceAlreadyExistsInDatabase
+	CouldNotFetchPasswordFromFrame (uint) = iota
 
 	// CouldNotFetchConditionFromFrame represents a could not fetch condition from frame
 	CouldNotFetchConditionFromFrame
@@ -43,14 +10,8 @@ const (
 	// CouldNotFetchMessageFromFrame represents a could not fetch message from frame
 	CouldNotFetchMessageFromFrame
 
-	// CouldNotFetchAccountFromFrame represents a could not fetch account from frame
-	CouldNotFetchAccountFromFrame
-
 	// CouldNotFetchRingFromFrame represents a could not fetch ring from frame
 	CouldNotFetchRingFromFrame
-
-	// CouldNotFetchGenerateRingFromFrame represents a could not fetch generate ring from frame
-	CouldNotFetchGenerateRingFromFrame
 
 	// CouldNotFetchCipherFromFrame represents a could not fetch cipher from frame
 	CouldNotFetchCipherFromFrame
@@ -60,12 +21,6 @@ const (
 
 	// CouldNotEncryptMessage represents a could not encrypt message
 	CouldNotEncryptMessage
-
-	// CouldNotRetrieveAccountFromDatabase represents a could not retrieve account from database
-	CouldNotRetrieveAccountFromDatabase
-
-	// CouldNotRetrieveAccountNamesListFromDatabase represents a could not retrieve account names list from database
-	CouldNotRetrieveAccountNamesListFromDatabase
 
 	// CouldNotFetchJoinVariableFromFrame represents a could not fetch a join variable from frame
 	CouldNotFetchJoinVariableFromFrame
@@ -88,12 +43,6 @@ const (
 	// CouldNotDecompileInstanceToBytes represents a could not fetch a decompile instance to bytes
 	CouldNotDecompileInstanceToBytes
 
-	// CouldNotFetchListQueryFromFrame represents a could not fetch list query from frame
-	CouldNotFetchListQueryFromFrame
-
-	// CouldNotFetchRetrieveQueryFromFrame represents a could not fetch retrieve query from frame
-	CouldNotFetchRetrieveQueryFromFrame
-
 	// CouldNotFetchListFromFrame represents a could not fetch list from frame
 	CouldNotFetchListFromFrame
 
@@ -114,42 +63,6 @@ const (
 
 	// CouldNotFetchCommitFromFrame represents a could not fetch commit from frame
 	CouldNotFetchCommitFromFrame
-
-	// CouldNotListInstancesFromDatabase represents a could not list instances from database
-	CouldNotListInstancesFromDatabase
-
-	// CouldNotRetrieveInstanceFromDatabase represents a could not retrieve instance from database
-	CouldNotRetrieveInstanceFromDatabase
-
-	// CouldNotBeginTransactionInDatabase represents a could not begin transaction in database
-	CouldNotBeginTransactionInDatabase
-
-	// CouldNotFetchQueryFromFrame represents a could not fetch query from frame
-	CouldNotFetchQueryFromFrame
-
-	// CouldNotCancelInDatabase represents a could not cancel in database service
-	CouldNotCancelInDatabase
-
-	// CouldNotCommitInDatabase represents a could not commit in database service
-	CouldNotCommitInDatabase
-
-	// CouldNotFetchIndexFromFrame could not fetch index from frame
-	CouldNotFetchIndexFromFrame
-
-	// CouldNotRevertToIndexInDatabase could not revert to index in database service
-	CouldNotRevertToIndexInDatabase
-
-	// CouldNotRevertInDatabase could not revert in database service
-	CouldNotRevertInDatabase
-
-	// CouldNotFetchInstanceFromFrame could not fetch instance from frame
-	CouldNotFetchInstanceFromFrame
-
-	// CouldNotInsertInDatabase could not insert in database service
-	CouldNotInsertInDatabase
-
-	// CouldNotDeleteFromDatabase could not delete from database service
-	CouldNotDeleteFromDatabase
 
 	// CouldNotFetchEncryptionPrivateKeyFromFrame could not fetch an encryption private key from frame
 	CouldNotFetchEncryptionPrivateKeyFromFrame
