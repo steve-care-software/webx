fbdd1e192c79563c1a7a22b434e287091a0ddbda
Steve Rodrigue <stev.rodr@gmail.com>
2024-05-30 15:09:32 -0400

    Removed unnecessary interfaces

--- domain/instances/sdk.go
+++ domain/instances/sdk.go
@@ -14,20 +14,3 @@ type Adapter interface {
 type Instance interface {
 	Hash() hash.Hash
 }
-
-// Repository represents an instance repository
-type Repository interface {
-	Dir(head hash.Hash, path []string) ([]string, error)
-	List(head hash.Hash, path []string) ([]string, error)
-	Exists(head hash.Hash, path []string) (Instance, error)
-	Retrieve(head hash.Hash, path []string) (Instance, error)
-}
-
-// Service represents an instance service
-type Service interface {
-	Begin(head hash.Hash) (*uint, error)
-	Insert(context uint, path []string, instance Instance) error
-	Delete(context uint, path []string) error
-	Commit(context uint) error
-	Cancel(context uint) error
-}
