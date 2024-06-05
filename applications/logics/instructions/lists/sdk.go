288d30eedec292c9eac36bfbc088eacbcd5d0c78
Steve Rodrigue <stev.rodr@gmail.com>
2024-06-05 13:35:21 -0400

    Added the list tests

--- applications/logics/instructions/lists/sdk.go
+++ applications/logics/instructions/lists/sdk.go
@@ -1,9 +1,22 @@
 package lists
 
 import (
+	"github.com/steve-care-software/datastencil/applications/logics/instructions/lists/deletes"
+	"github.com/steve-care-software/datastencil/applications/logics/instructions/lists/inserts"
 	"github.com/steve-care-software/datastencil/domain/instances/pointers/resources/logics/bridges/layers/instructions/lists"
 	"github.com/steve-care-software/datastencil/domain/stacks"
 )
+
+// NewApplication creates a new application
+func NewApplication(
+	insertApp inserts.Application,
+	deleteApp deletes.Application,
+) Application {
+	return createApplication(
+		insertApp,
+		deleteApp,
+	)
+}
 
 // Application represents an application
 type Application interface {
