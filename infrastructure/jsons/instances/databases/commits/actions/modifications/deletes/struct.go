3e83afc7f730cb34f020e35d926d0f8c7f33ed57
Steve Rodrigue <stev.rodr@gmail.com>
2024-07-01 17:26:11 -0400

    Removed database json adapter

--- infrastructure/jsons/instances/databases/commits/actions/modifications/deletes/struct.go
+++
@@ -1,7 +0,0 @@
-package deletes
-
-// Delete represents a delete struct
-type Delete struct {
-	Index  uint `json:"index"`
-	Length uint `json:"length"`
-}
--- infrastructure/jsons/instances/databases/heads/struct.go
+++
@@ -1,8 +0,0 @@
-package heads
-
-// Head represents an head
-type Head struct {
-	Path        []string `json:"path"`
-	Description string   `json:"description"`
-	IsActive    bool     `json:"is_active"`
-}
