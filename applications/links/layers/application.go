d90f270a033cc5e5674af2e855ae70db2cfecc91
Steve Rodrigue <stev.rodr@gmail.com>
2024-07-01 17:24:58 -0400

    Worked on the layer application

--- applications/links/layers/application.go
+++ applications/links/layers/application.go
@@ -5,30 +5,21 @@ import (
 	"github.com/steve-care-software/datastencil/applications/links/layers/instructions"
 	execution_layers "github.com/steve-care-software/datastencil/domain/instances/executions/links/layers"
 	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results"
-	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results/interruptions"
-	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results/interruptions/failures"
-	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results/success"
-	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results/success/outputs"
 	"github.com/steve-care-software/datastencil/domain/instances/layers"
 	"github.com/steve-care-software/datastencil/domain/stacks"
 )
 
 type application struct {
-	instructionsApp     instructions.Application
-	binaryApp           binaries.Application
-	stackFactory        stacks.Factory
-	stackBuilder        stacks.Builder
-	framesBuilder       stacks.FramesBuilder
-	frameBuilder        stacks.FrameBuilder
-	assignmentsBuilder  stacks.AssignmentsBuilder
-	assignmentBuilder   stacks.AssignmentBuilder
-	assignableBuilder   stacks.AssignableBuilder
-	layerBuilder        execution_layers.LayerBuilder
-	resultBuilder       results.Builder
-	interruptionBuilder interruptions.Builder
-	failureBuilder      failures.Builder
-	successBuilder      success.Builder
-	outputBuilder       outputs.Builder
+	instructionsApp    instructions.Application
+	binaryApp          binaries.Application
+	stackFactory       stacks.Factory
+	stackBuilder       stacks.Builder
+	framesBuilder      stacks.FramesBuilder
+	frameBuilder       stacks.FrameBuilder
+	assignmentsBuilder stacks.AssignmentsBuilder
+	assignmentBuilder  stacks.AssignmentBuilder
+	assignableBuilder  stacks.AssignableBuilder
+	layerBuilder       execution_layers.LayerBuilder
 }
 
 func createApplication(
@@ -43,27 +34,18 @@ func createApplication(
 	assignableBuilder stacks.AssignableBuilder,
 	layerBuilder execution_layers.LayerBuilder,
 	resultBuilder results.Builder,
-	interruptionBuilder interruptions.Builder,
-	failureBuilder failures.Builder,
-	successBuilder success.Builder,
-	outputBuilder outputs.Builder,
 ) Application {
 	out := application{
-		instructionsApp:     instructionsApp,
-		binaryApp:           binaryApp,
-		stackFactory:        stackFactory,
-		stackBuilder:        stackBuilder,
-		framesBuilder:       framesBuilder,
-		frameBuilder:        frameBuilder,
-		assignmentsBuilder:  assignmentsBuilder,
-		assignmentBuilder:   assignmentBuilder,
-		assignableBuilder:   assignableBuilder,
-		layerBuilder:        layerBuilder,
-		resultBuilder:       resultBuilder,
-		interruptionBuilder: interruptionBuilder,
-		failureBuilder:      failureBuilder,
-		successBuilder:      successBuilder,
-		outputBuilder:       outputBuilder,
+		instructionsApp:    instructionsApp,
+		binaryApp:          binaryApp,
+		stackFactory:       stackFactory,
+		stackBuilder:       stackBuilder,
+		framesBuilder:      framesBuilder,
+		frameBuilder:       frameBuilder,
+		assignmentsBuilder: assignmentsBuilder,
+		assignmentBuilder:  assignmentBuilder,
+		assignableBuilder:  assignableBuilder,
+		layerBuilder:       layerBuilder,
 	}
 
 	return &out
@@ -71,12 +53,12 @@ func createApplication(
 
 // ExecuteWithInput executes the layer using the provided input and returns the executions
 func (app *application) ExecuteWithInput(layer layers.Layer, input []byte) (execution_layers.Layer, error) {
-	return nil, nil
+	return app.execute(layer, input)
 }
 
 // Execute executes a layer
 func (app *application) Execute(layer layers.Layer) (execution_layers.Layer, error) {
-	return nil, nil
+	return app.execute(layer, nil)
 }
 
 func (app *application) execute(layer layers.Layer, input []byte) (execution_layers.Layer, error) {
@@ -86,59 +68,16 @@ func (app *application) execute(layer layers.Layer, input []byte) (execution_lay
 	}
 
 	instructions := layer.Instructions()
-	retStack, pStopLine, pCode, err := app.instructionsApp.Execute(stack, instructions)
+	result, err := app.instructionsApp.Execute(stack, instructions)
 	if err != nil {
 		return nil, err
 	}
 
-	isStop := pStopLine != nil
-	isFailure := pCode != nil
-	if isStop || isFailure {
-		interruptionBuilder := app.interruptionBuilder.Create()
-		if isStop {
-			interruptionBuilder.WithStop(*pStopLine)
-		}
-
-		if isFailure {
-
-		}
-	}
-
-	output := layer.Output()
-	variable := output.Variable()
-	retBytes, err := retStack.Head().FetchBytes(variable)
-	if err != nil {
-		return nil, err
-	}
-
-	outputBuilder := app.outputBuilder.Create().WithInput(retBytes)
-	if output.HasExecute() {
-		execute := output.Execute()
-		retAfterExecBytes, err := app.binaryApp.Execute(retBytes, execute)
-		if err != nil {
-			return nil, err
-		}
-
-		outputBuilder.WithExecute(retAfterExecBytes)
-	}
-
-	retOutput, err := outputBuilder.Now()
-	if err != nil {
-		return nil, err
-	}
-
-	kind := output.Kind()
-	success, err := app.successBuilder.Create().WithKind(kind).WithOutput(retOutput).Now()
-	if err != nil {
-		return nil, err
-	}
-
-	result, err := app.resultBuilder.Create().WithSuccess(success).Now()
-	if err != nil {
-		return nil, err
-	}
-
-	return app.layerBuilder.Create().WithInput(input).WithResult(result).WithSource(layer).Now()
+	return app.layerBuilder.Create().
+		WithInput(input).
+		WithSource(layer).
+		WithResult(result).
+		Now()
 }
 
 func (app *application) stack(layer layers.Layer, input []byte) (stacks.Stack, error) {
--- applications/links/layers/instructions/sdk.go
+++ applications/links/layers/instructions/sdk.go
@@ -1,11 +1,12 @@
 package instructions
 
 import (
+	"github.com/steve-care-software/datastencil/domain/instances/executions/links/layers/results"
 	"github.com/steve-care-software/datastencil/domain/instances/layers/instructions"
 	"github.com/steve-care-software/datastencil/domain/stacks"
 )
 
 // Application represents an instructions application
 type Application interface {
-	Execute(stack stacks.Stack, instructions instructions.Instructions) (stacks.Stack, *uint, *uint, error)
+	Execute(stack stacks.Stack, instructions instructions.Instructions) (results.Result, error)
 }
