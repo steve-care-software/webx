47a3b186133ed7a2e018dce7ea0cd025ae19ec25
Steve Rodrigue <stev.rodr@gmail.com>
2024-07-14 00:17:28 -0400

    Added the input structs

--- domain/instances/layers/instructions/assignments/assignables/executions/executes/inputs/sdk.go
+++ domain/instances/layers/instructions/assignments/assignables/executions/executes/inputs/sdk.go
@@ -1,7 +1,28 @@
 package inputs
 
+import (
+	"github.com/steve-care-software/historydb/domain/hash"
+)
+
+// NewBuilder creates a new builder
+func NewBuilder() Builder {
+	hashAdapter := hash.NewAdapter()
+	return createBuilder(
+		hashAdapter,
+	)
+}
+
+// Builder represents the input builder
+type Builder interface {
+	Create() Builder
+	WithValue(value string) Builder
+	WithPath(path string) Builder
+	Now() (Input, error)
+}
+
 // Input represents an input
 type Input interface {
+	Hash() hash.Hash
 	IsValue() bool
 	Value() string
 	IsPath() bool
