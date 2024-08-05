ba629cf3131c36ba653856d93209d55f8ec9a7f3
Steve Rodrigue <stev.rodr@gmail.com>
2024-08-05 19:25:57 -0400

    Added install methods

--- engine/bytes/applications/cursors/sdk.go
+++ engine/bytes/applications/cursors/sdk.go
@@ -1,10 +1,15 @@
 package cursors
 
 import (
+	"github.com/steve-care-software/webx/engine/bytes/domain/branches"
 	"github.com/steve-care-software/webx/engine/bytes/domain/cursors"
 	"github.com/steve-care-software/webx/engine/bytes/domain/cursors/status"
 	"github.com/steve-care-software/webx/engine/bytes/domain/delimiters"
+	"github.com/steve-care-software/webx/engine/bytes/domain/iterations"
+	"github.com/steve-care-software/webx/engine/bytes/domain/namespaces"
 	"github.com/steve-care-software/webx/engine/bytes/domain/originals"
+	"github.com/steve-care-software/webx/engine/bytes/domain/versions"
+	"github.com/steve-care-software/webx/engine/bytes/domain/workspaces"
 )
 
 // Builder represents the application builder
@@ -16,14 +21,24 @@ type Builder interface {
 
 // Application represents the cursor application
 type Application interface {
-	Cursor() cursors.Cursor            // returns the current cursor
-	Records() (status.Statuses, error) // returns the recorded cursors
-	Erase(name string) error           // erase a cursor using its name
-	Record(name string) error          // record the cursor to this name
-	Replace(name string) error         // put the cursor to what the name was pointing to
-	Set(name string) error             // set the cursor to this element (horizontally)
-	Down(name string) error            // set the cursor to this element (sub-element or 'down')
-	Climb(name string) error           // set the cursor to this element (parent-element or 'climb')
+	Cursor() cursors.Cursor                  // returns the current cursor
+	Records() (status.Statuses, error)       // returns the recorded cursors
+	MetaData() (delimiters.Delimiter, error) // returns the current branch meta data
+
+	// install:
+	InstallNamespaces(namespaces namespaces.Namespaces) error
+	InstallVersions(versions versions.Versions) error
+	InstallIterations(iterations iterations.Iterations) error
+	InstallWorkspaces(workspaces workspaces.Workspaces) error
+	InstallBranches(branches branches.Branches) error
+
+	// write:
+	Erase(name string) error   // erase a cursor using its name
+	Record(name string) error  // record the cursor to this name
+	Replace(name string) error // put the cursor to what the name was pointing to
+	Set(name string) error     // set the cursor to this element (horizontally)
+	Down(name string) error    // set the cursor to this element (sub-element or 'down')
+	Climb(name string) error   // set the cursor to this element (parent-element or 'climb')
 	Insert(original originals.Original) error
 	Update(original string, updated originals.Original) error
 	Delete(name string) error
@@ -31,7 +46,6 @@ type Application interface {
 	Purge(name string) error
 	PurgeAll() error
 	Move(name string, devName string, deleteOriginal bool) error // moves a development iteration to a production iteration inside the current iteration
-	MetaData() (delimiters.Delimiter, error)                     // returns the current branch meta data
 	Merge(deleteOriginal bool) error                             // merge the current branch into its parent and set its parent as the current branch
 
 	// data:
--- engine/bytes/domain/workspaces/sdk.go
+++ engine/bytes/domain/workspaces/sdk.go
@@ -1,1 +1,10 @@
 package workspaces
+
+// Workspaces represents workspaces
+type Workspaces interface {
+	List() []Workspace
+}
+
+// Workspace represents a workspace
+type Workspace interface {
+}
