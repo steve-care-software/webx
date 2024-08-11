5dc73ff896df9286db616f6a81b02f2d7548242d
Steve Rodrigue <stev.rodr@gmail.com>
2024-08-11 13:26:09 -0400

    Added placeholder files

---
+++ engine/cursors/infrastructure/applications/identity_test.go
@@ -0,0 +1,23 @@
+package applications
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/steve-care-software/webx/engine/cursors/applications/sessions/loaders/identities"
+	applications_loaders "github.com/steve-care-software/webx/engine/cursors/applications/sessions/loaders/namespaces/versions/workspaces/branches/states/pointers"
+	"github.com/steve-care-software/webx/engine/cursors/infrastructure/edwards25519"
+)
+
+func TestIdentity_Success(t *testing.T) {
+	encryptionApp := edwards25519.NewEncryptionApplication()
+	storagePointerApp := applications_loaders.NewApplication()
+	identityApp := identities.NewApplication(
+		encryptionApp,
+		storagePointerApp,
+		nil,
+		4096,
+	)
+
+	fmt.Printf("\n%v\n", identityApp)
+}
