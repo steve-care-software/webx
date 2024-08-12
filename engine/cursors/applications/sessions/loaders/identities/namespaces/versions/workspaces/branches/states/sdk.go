819121cd91219d55ea97ab91a9947b295f5f0b18
Steve Rodrigue <stev.rodr@gmail.com>
2024-08-12 16:59:10 -0400

    Added the flag and kind methods

--- engine/cursors/applications/sessions/loaders/identities/namespaces/versions/workspaces/branches/states/sdk.go
+++ engine/cursors/applications/sessions/loaders/identities/namespaces/versions/workspaces/branches/states/sdk.go
@@ -32,6 +32,8 @@ type Application interface {
 	List(input resources.Resource, kind uint8) ([]string, error)                        // list all the states of the branch
 	Select(input resources.Resource, name string) (resources.Resource, error)           // selects the state by name
 	Set(input resources.Resource, flag uint8) (resources.Resource, error)               // sets the pointer to the resource, list or entity
+	Flag() (*uint, error)                                                               // returns the current selected flag
+	Kind() (*uint, error)                                                               // returns the current selected kind
 	Delete(input resources.Resource) (resources.Resource, error)                        // deletes the current state
 	Recover(input resources.Resource) (resources.Resource, error)                       // recovers the current state
 	Entities(input resources.Resource) (entities.Application, error)                    // returns the entity application with the current state's entities
