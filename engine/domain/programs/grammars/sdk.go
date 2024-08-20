5b690db2bbf3c832a11f7af62da0e8bcfe38d48d
Steve Rodrigue <stev.rodr@gmail.com>
2024-08-20 18:14:46 -0400

    Added the program and grammar adapter interfaces

--- engine/domain/programs/grammars/sdk.go
+++ engine/domain/programs/grammars/sdk.go
@@ -1,6 +1,7 @@
 package grammars
 
 import (
+	"github.com/steve-care-software/webx/engine/domain/nfts"
 	"github.com/steve-care-software/webx/engine/domain/programs/grammars/blocks"
 	"github.com/steve-care-software/webx/engine/domain/programs/grammars/blocks/lines/tokens/elements"
 	"github.com/steve-care-software/webx/engine/domain/programs/grammars/rules"
@@ -10,6 +11,30 @@ import (
 // NewBuilder creates a new builder
 func NewBuilder() Builder {
 	return createBuilder()
+}
+
+// ParserAdapter represents the grammar parser adapter
+type ParserAdapter interface {
+	// ToGrammar takes the input and converts it to a grammar instance and the remaining data
+	ToGrammar(input []byte) (Grammar, []byte, error)
+
+	// ToBytes takes a grammar and returns the bytes
+	ToBytes(grammar Grammar) ([]byte, error)
+}
+
+// NFTAdapter represents the grammar nft adapter
+type NFTAdapter interface {
+	// ToNFT converts a grammar instance to an NFT
+	ToNFT(grammar Grammar) (nfts.NFT, error)
+
+	// ToGrammar converts an NFT to a grammar instance
+	ToGrammar(nft nfts.NFT) (Grammar, error)
+}
+
+// ComposeAdapter represents the grammar compose adapter
+type ComposeAdapter interface {
+	// ToBytes takes a grammar and a blockname and returns its bytes
+	ToBytes(grammar Grammar, blockName string) ([]byte, error)
 }
 
 // Builder represents the grammar builder
--- engine/domain/programs/sdk.go
+++ engine/domain/programs/sdk.go
@@ -1,10 +1,35 @@
 package programs
 
 import (
+	"github.com/steve-care-software/webx/engine/domain/nfts"
 	"github.com/steve-care-software/webx/engine/domain/programs/grammars"
 	"github.com/steve-care-software/webx/engine/domain/programs/instructions"
 	"github.com/steve-care-software/webx/engine/domain/programs/instructions/tokens/elements"
 )
+
+// ParserAdapter represents the program parser adapter
+type ParserAdapter interface {
+	// ToProgram takes the grammar and input and converts them to a program instance and the remaining data
+	ToProgram(grammar grammars.Grammar, input []byte) (Program, []byte, error)
+
+	// ToBytes takes a program and returns the bytes for the grammar and program
+	ToBytes(program Program) ([]byte, []byte, error)
+}
+
+// NFTAdapter represents the program nft adapter
+type NFTAdapter interface {
+	// ToNFT converts a program instance to an NFT
+	ToNFT(program Program) (nfts.NFT, error)
+
+	// ToProgram converts an NFT to a program instance
+	ToProgram(nft nfts.NFT) (Program, error)
+}
+
+// ComposeAdapter represents the program compose adapter
+type ComposeAdapter interface {
+	// ToBytes takes a program and returns its bytes
+	ToBytes(program Program) ([]byte, error)
+}
 
 // Builder represents the program builder
 type Builder interface {
